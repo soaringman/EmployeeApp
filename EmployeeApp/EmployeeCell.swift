c254c5b0ef0617facded1b53307fec4596fa27d9
Alexey Gulyaev <sargonmeister@gmail.com>
2022-11-09 00:03:03 +0300

    add real data to application

--- EmployeeApp/EmployeeCell.swift
+++ EmployeeApp/EmployeeCell.swift
@@ -6,23 +6,64 @@
 //
 
 import UIKit
+import SnapKit
 
 class EmployeeCell: UITableViewCell {
     
     //MARK: - static properties
     static let reuseID = "EmployeeCell"
     
+    
+    private lazy var nameLabel = UILabel()
+    private lazy var skillsLabel = UILabel()
+    private lazy var phoneNumberLabel = UILabel()
+    
     //MARK: - Initialization
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: EmployeeCell.reuseID)
-
+        
+        setupUI()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupUI() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+        contentView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.top.equalToSuperview().inset(10)
+        }
+        
+        contentView.addSubview(skillsLabel)
+        skillsLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.top.equalToSuperview().inset(30)
+        }
+        
+        contentView.addSubview(phoneNumberLabel)
+        phoneNumberLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.top.equalTo(nameLabel.snp.bottom).offset(16)
+        }
+    }
+    
+    func configure(name: String,
+                   phoneNumber: String,
+                   skills: [String]
+    ) {
+        nameLabel.text = name
+        phoneNumberLabel.text = phoneNumber
+        skillsLabel.text = skills.joined(separator: ", ")
+    }
     
     
 }
--- EmployeeApp/EmployeeViewController.swift
+++ EmployeeApp/EmployeeViewController.swift
@@ -11,12 +11,12 @@ class EmployeeViewController: UIViewController {
     
     //MARK: - private properties
     
+    private var employee: [EmployeeElement] = []
+    private var company = ""
+    private let networkManger = NetworkManager()
     
     //MARK: - ui elements
     private lazy var tableView = UITableView(frame: UIScreen.main.bounds, style: .grouped)
-    
-    var networkManger = NetworkManager()
-    
     
     //MARK: - life cicle
     override func viewDidLoad() {
@@ -25,7 +25,7 @@ class EmployeeViewController: UIViewController {
         setupTable()
         setupUI()
         
-        networkManger.fetchData(url: "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c")
+        getData()
     }
     
     //MARK: - private methods
@@ -34,14 +34,18 @@ class EmployeeViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(EmployeeCell.self, forCellReuseIdentifier: EmployeeCell.reuseID)
+        tableView.rowHeight = 100
     }
     
     private func setupUI() {
         self.view.addSubview(tableView)
     }
     
-    private func setupConstraints() {
-        
+    func getData() {
+        let api = Api()
+        let baseURL = api.baseURL
+        networkManger.fetchData(url: baseURL)
+        networkManger.delegate = self
     }
 }
 
@@ -53,17 +57,19 @@ extension EmployeeViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Avito"
+        return self.company
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return employee.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: EmployeeCell.reuseID, for: indexPath) as? EmployeeCell {
-            
+            let data = employee[indexPath.row]
+            cell.configure(name: data.name, phoneNumber: data.phoneNumber, skills: data.skills)
             return cell
+            
         }
         return UITableViewCell()
     }
@@ -71,6 +77,30 @@ extension EmployeeViewController: UITableViewDataSource {
 
     //MARK: - UITableViewDelegate
 extension EmployeeViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
+//
+//
+//
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
+}
+
+extension EmployeeViewController: NetworkManagerDelegate {
+    
+    func showData(result: Company?) {
+        guard let result = result else {return}
+        self.company = result.name
+        self.employee = result.employees
+        tableView.reloadData()
+    }
+    
+    func showError() {
+        print("Error recive data")
+    }
+    
     
 }
 
